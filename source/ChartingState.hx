d0688df7fdce389a3d7844c7282dfc3afbf7c9c2
RecoloredPoyo4 <82355015+RecoloredPoyo4@users.noreply.github.com>
2022-05-30 07:03:44 -0400

    update ChartingState.hx

--- source/ChartingState.hx
+++ source/ChartingState.hx
@@ -113,6 +113,9 @@ class ChartingState extends MusicBeatState
 
 	override function create()
 	{
+	  Paths.clearStoredMemory();
+		Paths.clearUnusedMemory();
+
 		curSection = lastSection;
 
 		if (PlayState.SONG != null)
